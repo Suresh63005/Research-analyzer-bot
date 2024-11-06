f368e79168ca4bf2b363194ffff9d5f48539c56b
Suresh63005 <193j5a0335@raghuinstech.com>
2024-11-06 09:41:33 +0530

    size limit increase

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8081;
 
 const upload = multer({ 
     storage: multer.memoryStorage(),
-    limits: { fileSize: 5 * 1024 * 1024 } 
+    limits: { fileSize: 20 * 1024 * 1024 } 
 });
 
 const app = express();
